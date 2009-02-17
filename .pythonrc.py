7682f118c72352507f751a95cc7e96eac7791c73
Liam Cooke <noreply@liamcooke.com>
2009-02-17 00:39:09 +0000

    try/catch when importing readline

--- .pythonrc.py
+++ .pythonrc.py
@@ -15,4 +15,7 @@ sys.ps2 = '. '
 def enable_tab_completion():
     import rlcompleter, readline
     readline.parse_and_bind("tab: complete")
-enable_tab_completion()
+try:
+    enable_tab_completion()
+except:
+    print "No tab completion. :("

a4562a2c81789d987ed3b13287b4c689d425dda7
Liam Cooke <noreply@liamcooke.com>
2017-09-22 21:45:36 +1000

    cdfinder doesn't work as a script

--- bash/platform_macos.bash
+++ bash/platform_macos.bash
@@ -18,8 +18,15 @@ export HOMEBREW_NO_INSECURE_REDIRECT=1
 export PATH="$PATH:$HOME/.myconf/macos/bin"
 
 # -----------------------------------------------------------------------------
-# Aliases
+# Aliases & functions
 # -----------------------------------------------------------------------------
 
 # Open MacVim in the foreground without forking.
 alias fvim="/usr/local/bin/mvim -f -c 'au VimLeave * !open -a Terminal'"
+
+# cd to the top-most Finder window location
+# Originally from https://github.com/mathiasbynens/dotfiles
+cdfinder() {
+    cd "$(osascript -e \
+        'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
